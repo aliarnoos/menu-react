ec82963d15e503d09e2c02a48a89e275d1f88f0c
aliarnoos <alimarnoos@gmail.com>
2022-08-11 00:00:57 +0300

    finish the project with adding navigation function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
 import Card from './components/Card';
 import './styles.css'
+import data from './data'
 
-class App extends Component {
-  state = {  }
- 
-  render() { 
+function App () {
+  const cards = data.map(card => <Card key={card.id} title={card.title} category={card.category} price={card.price} img={card.img} desc={card.desc} />)
+  const [menu, setMenu] = React.useState(cards)
 
+  const filterCards = (cate) => {
+    const newdata = data.filter(card => card.category === cate );
+    const newCards = newdata.map(card =><Card key={card.id} title={card.title} category={card.category} price={card.price} img={card.img} desc={card.desc} />)
+    setMenu(newCards)
+  }
+  const allCards = () =>{
+    setMenu(cards)
+    }
     return (
       <div className='main'>
         <div className='nav-btns'>
-          <button>All</button>
-          <button>Breakfast</button>
-          <button>Lunch</button>
-          <button>Shakes</button>
+          <button onClick={allCards}>All</button>
+          <button onClick={()=> filterCards('breakfast')}>Breakfast</button>
+          <button onClick={()=> filterCards('lunch')}>Lunch</button>
+          <button onClick={()=> filterCards('shakes')}>Shakes</button>
         </div>
         <div className='main-div'>
-          <Card/>         
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-
+          {menu}
         </div>
       </div>
     );
   }
-}
+
 
 export default App;
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,17 +7,16 @@ class Card extends Component {
         return (
             <div className='card-item'>
                 <div className='img-div'>
-                    <img src='https://react-projects-5-menu.netlify.app/images/item-1.jpeg' alt=''/>
+                    <img src={this.props.img} alt='food picture'/>
                 </div>
                 <div className='text-div'>
                     <div className='header-text'>
-                        <h4>Buttermilk Pancakes</h4>
-                        <h4>15 $</h4>
+                        <h4>{this.props.title}</h4>
+                        <h4 className='price'>{this.props.price}$</h4>
                     </div>
                     <div className='body-text'>
                     <p className='dots-line'>.............................................................</p>
-                    I'm baby woke mlkshk wolf bitters live-edge blue bottle, 
-                    hammock freegan copper mug whatever cold-pressed       
+                        {this.props.desc}
                     </div>
                 </div>
             </div>
